672ee09c16c85deed36c751a9669da0b9df15391
Zach Leatherman <zachleatherman@gmail.com>
2022-01-31 15:00:57 -0600

    Use :defined instead of a JS class.

--- details-utilities.js
+++ details-utilities.js
@@ -249,7 +249,6 @@ class DetailsUtils extends HTMLElement {
 			closeClickOutside: "close-click-outside",
 			forceStateClosed: "force-closed",
 			modal: "modal",
-			js: "js",
 		};
 
 		this.options = {};
@@ -287,8 +286,6 @@ class DetailsUtils extends HTMLElement {
 			return;
 		}
 		this.initialized = true;
-
-		this.setAttribute(this.attrs.js, "");
 
 		this.options.closeClickOutside = this.getAttributeValue(this.attrs.closeClickOutside);
 		this.options.closeEsc = this.getAttributeValue(this.attrs.closeEsc);
